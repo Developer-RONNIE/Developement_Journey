3bc0f6eb8e87516dd6fca74cc3d405f4f544e0d8
Developer-RONNIE <ronnieworkspace@gmail.com>
2026-09-08 13:46:01 +0530

    Add initial C and C++ source files and .gitignore

---
+++ 01_C_Language/02_DataTypes.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int main() {
+
+    int num = 12;                // integer data type
+    char letter = 'A';           // character data type
+    float decimal = 3.14;        // float data type
+
+    // print the values of data types
+    printf("Integer: %d\n", num);
+    printf("Character: %c\n", letter);
+    printf("Float: %f\n", decimal);
+    return 0;
+}

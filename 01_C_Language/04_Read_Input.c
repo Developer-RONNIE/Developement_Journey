be85307dde7dabf3be1b2a7ce6d44e2f9678d02e
Developer-RONNIE <ronnieworkspace@gmail.com>
2026-09-13 16:30:16 +0530

    Add C program for reading integer and character input from user

---
+++ 01_C_Language/04_Read_Input.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+    int num1;
+    char ch;
+
+
+    printf("Enter an integer: ");
+    scanf("%d", &num1);              // read an integer from user input
+
+    // Buffer clearing to avoid reading newline character left in the input buffer
+    printf("Enter a character: ");
+    scanf(" %c", &ch);                // read a character from user input
+
+    printf("You entered Integer Value of: %d\n", num1); // print the entered integer
+    printf("You entered Character Value of: %c\n", ch);     // print the entered character
+    return 0;
+
+}

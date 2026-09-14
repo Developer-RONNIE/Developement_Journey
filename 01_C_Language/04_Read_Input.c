87197a2a355b88cfb20911bc0c27437aa94f7cb9
Developer-RONNIE <ronnieworkspace@gmail.com>
2026-09-14 05:37:28 +0530

    Clarify buffer clearing in input reading program

--- 01_C_Language/04_Read_Input.c
+++ 01_C_Language/04_Read_Input.c
@@ -10,8 +10,9 @@ int main() {
     scanf("%d", &num1);              // read an integer from user input
 
     // Buffer clearing to avoid reading newline character left in the input buffer
+    // or use getchar() here to consume the newline character
     printf("Enter a character: ");
-    scanf(" %c", &ch);                // read a character from user input
+    scanf(" %c", &ch);                // read a character from user input 
 
     printf("You entered Integer Value of: %d\n", num1); // print the entered integer
     printf("You entered Character Value of: %c\n", ch);     // print the entered character
